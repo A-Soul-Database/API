929b0a54728896d323c127e54701db30a1d0fa5f
PPPPP <32994395+P-PPPP@users.noreply.github.com>
2022-02-12 16:31:51 +0800

    已经整合进 router里面啦

--- Subtitle-API/README.md
+++
@@ -1,5 +0,0 @@
-字幕库搜索的API
-
-运行方式：python main.py
-
-在config.py里修改data_path,将其改为服务器上A-Soul-Data这一仓库的地址即可
--- Subtitle-API/config.py
+++
@@ -1,5 +0,0 @@
-configs = {
-    'data_path': 'https://raw.githubusercontent.com/A-Soul-Database/A-Soul-Data', # path to local Asoul-Soul-Data or https://raw.githubusercontent.com/A-Soul-Database/A-Soul-Data
-    'running_type':'remote', # remote / local
-    'port': 8001
-} #开发环境下的配置
