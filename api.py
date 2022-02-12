29e23565aaa54c01fcf2ad56488891029b760466
PPPPP <32994395+P-PPPP@users.noreply.github.com>
2022-02-12 16:32:30 +0800

    Update api.py

--- api.py
+++ api.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 from threading import Thread
 from depedencies import Get_Source
 from routers import List_Json , Subtitle_Search , etc
-Api = FastAPI()
-Api.include_router(List_Json.Api)
-Api.include_router(Subtitle_Search.app)
-Api.include_router(etc.Api)
+Api = FastAPI(
+        docs_url="/Api/docs",
+        openapi_url="/Api/openapi"
+        )
+Api.include_router(List_Json.Api,prefix="/Api")
+Api.include_router(Subtitle_Search.app,prefix="/Api")
+Api.include_router(etc.Api,prefix="/Api")
 
 if __name__ == "__main__":
     Thread(target=Get_Source.Sync_Data).start()
